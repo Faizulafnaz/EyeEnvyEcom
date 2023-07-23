d40ee75e1a79c68c2b0b766f42678cbfd875a29f
Faizulafnaz <faizulafnaz22@gmail.com>
2023-07-23 11:20:50 +0530

    filter function added

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.store, name='store'),
-    path('<slug:product_slug>/',views.product_details, name='product_details'),
-]   
+    path('product/<slug:product_slug>/',views.product_details, name='product_details'),
+    path('filtered-product/', views.filtered_products, name='filtered_products'),
+]       
--- store/views.py
+++ store/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import redirect, render
-from .models import Product
+from .models import Product, Brand
 from wishlist.models import Wishlist
 from offers.models import Offer
 from category.models import Category
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 
 # Create your views here.
@@ -11,13 +12,12 @@ from django.contrib.auth.decorators import login_required
 
 def store(request):
     products = Product.objects.all().filter(is_available = True)
-    try:
-        pass
-    except:
-        pass
+    categories = Category.objects.all()
+    brand = Brand.objects.all()
     context = {
         'products' : products,
-        
+        'categories' : categories,
+        'brands' : brand,
     }
     return render(request, 'homeapp/shop.html', context)
 
@@ -43,5 +43,52 @@ def product_details(request, product_slug):
     }
     return render(request, 'homeapp/single.html', context)
 
+
+def filtered_products(request):
+    # Retrieve the filter options selected by the user from the URL query parameters
+    selected_categories = request.GET.getlist('category')
+    selected_brands = request.GET.getlist('brand')
+    search_query = request.GET.get('q')
+
+
+    # Query the database to get the filtered products
+    filtered_products = Product.objects.all().filter(is_available = True)
+    count = 0
+    c = 0
+    if search_query:
+        count += 1
+        filtered_products = filtered_products.filter(
+            Q(product_name__icontains=search_query) |
+            Q(description__icontains=search_query)
+        )
+        c = filtered_products.count()
+    if selected_categories:
+        filtered_products = filtered_products.filter(category__in=selected_categories)
+        c = filtered_products.count()
+        count += 1
+
+    if selected_brands:
+        filtered_products = filtered_products.filter(brand__in=selected_brands)
+        count += 1
+        c = filtered_products.count()
+    
+    if count == 0:
+        filtered_products = None
+
+    
+
+
+    categories = Category.objects.all()
+    brand = Brand.objects.all()
+
+    context = {
+        'products' : filtered_products,
+        'categories' : categories,
+        'brands' : brand,
+        'c' : c,
+        'f': True,
+    }
+
+    return render(request, 'homeapp/shop.html', context)
 
 
